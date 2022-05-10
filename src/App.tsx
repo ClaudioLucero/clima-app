fb1a82d8ca2af61944537599d3b51548076f87c6
cluceroSooft <clucero@sooft.com.ar>
2022-05-09 23:58:29 -0300

    create  project && first component && config Eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SearchCities from './components/SearchCities/SearchCities';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <SearchCities />
+      </div>
+      <div className="App-body" />
     </div>
   );
 }
---
+++ src/components/SearchCities/SearchCities.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
+import React from 'react';
+import './SearchCities.css';
+
+function SearchCities() {
+  return (
+    <div className="search-cities">
+      Hola
+    </div>
+  );
+}
+
+export default SearchCities;
