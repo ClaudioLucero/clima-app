77e6228c0667759cb3b94f07542d22e3ce47c60a
ClaudioLucero <clucero@sooft.com.ar>
2022-05-13 01:56:08 -0300

    Favorites : implement props and state && functions add and delete

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -1,25 +1,50 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import React, { useEffect, useState } from 'react';
 import './Favorites.css';
-import { TiDeleteOutline } from 'react-icons/ti';
 import { City } from '../../models/City';
 
 interface Props {
-  fav:City[]
+  fav: City[],
+  callbackCurrentCity:Function
 }
 interface AppState {
-  myFavorites:City[]
+  myFavorites: City[]
 }
-const Favorites:React.FC<Props> = ({ fav }:Props) => {
+const Favorites: React.FC<Props> = ({ fav, callbackCurrentCity }: Props) => {
   const [favs, setFavs] = useState<AppState['myFavorites']>([]);
   useEffect(() => {
     setFavs(fav);
   }, [fav]);
-  // eslint-disable-next-line no-trailing-spaces
 
+  const deleteFavorite = (city:string) => {
+    setFavs(favs.filter((item) => item.name !== city));
+  };
+  const selectFavorite = (city:City) => {
+    callbackCurrentCity(city);
+  };
   const cities = favs.map((city) => (
     <div className="favourites-city" key={city.id}>
-      {city.name}
-      <TiDeleteOutline />
+      <div
+        className="favourites-city-name"
+        role="button"
+        title={city.name}
+        tabIndex={0}
+        onClick={() => { selectFavorite(city); }}
+        onKeyDown={() => { selectFavorite(city); }}
+      >
+        {city.name}
+      </div>
+      <div
+        className="favourites-city-icon"
+        role="button"
+        tabIndex={0}
+        title={city.name}
+        onClick={() => { deleteFavorite(city.name); }}
+        onKeyDown={() => { deleteFavorite(city.name); }}
+      >
+        X
+      </div>
     </div>
   ));
 
