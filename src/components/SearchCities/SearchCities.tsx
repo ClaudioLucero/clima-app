1c5dd6f0f2a9da5671096844502fe463214d9923
ClaudioLucero <clucero@sooft.com.ar>
2022-05-13 01:57:35 -0300

    SearchCities: create function search city in json

--- src/components/SearchCities/SearchCities.tsx
+++ src/components/SearchCities/SearchCities.tsx
@@ -2,21 +2,28 @@
 import React, { Fragment, useState } from 'react';
 import { City } from '../../models/City';
 import ResultsCity from '../ResultsCity/ResultsCity';
+import { searchProvice } from '../../services/searchProvices.js';
 import './SearchCities.css';
 
 interface Props {
-  fav:City[]
+  fav: City[]
 }
 
-const SearchCities : React.FC<Props> = ({ fav }:Props) => {
+const SearchCities: React.FC<Props> = ({ fav }: Props) => {
   const [myCities, setMyCities] = useState<Array<City>>([]);
-  const handleInput = (_evt: React.MouseEvent<HTMLInputElement, MouseEvent>) => {
-    setMyCities(fav);
+  const [valueSearch, setValueSearch] = useState('');
+  const handleInput = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    // setMyCities(fav);
+    setValueSearch(evt.target.value);
+    const { value } = evt.target;
+    if (value.length > 2) {
+      searchProvice(value.toLocaleUpperCase());
+    }
   };
 
   return (
     <div className="search-cities">
-      <input className="search-cities-input" onClick={(_evt) => { handleInput(_evt); }} />
+      <input className="search-cities-input" onChange={handleInput} value={valueSearch} />
       {myCities.length > 0 && (
         <div />
       )}
